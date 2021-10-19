b1b7d40b1b462e9614cd62cf58d25733c1388192
taifatang <taifatang@hotmail.com>
2021-10-19 19:05:32 +0100

    update rover with complex movements

--- src/PlutoRover/PlutoRover.cs
+++ src/PlutoRover/PlutoRover.cs
@@ -5,44 +5,71 @@ namespace PlutoRover
 
     public class PlutoRover
     {
+        private int _orientation;
         public int X { get; private set; }
         public int Y { get; private set; }
-        public int Orientation { get; private set; }
+
+        public int Orientation
+        {
+            get => _orientation % 360;
+        }
 
         public PlutoRover(int x, int y, int orientation)
         {
             X = x;
             Y = y;
-            Orientation = orientation;
+            _orientation = orientation;
         }
 
         private void Turn(char direction)
         {
             if (direction == 'L')
             {
-                Orientation -= 90;
+                _orientation += 270;
             }
             else
             {
-                Orientation += 90;
+                _orientation += 90;
             }
 
         }
+
+        private Direction GetCurrentDirection()
+        {
+            return (Direction)Math.Abs(Orientation / 90 % 4);
+        }
+
         public void Execute(string s)
         {
             foreach (char command in s)
             {
                 if (command == 'F')
                 {
-                    X += 1;
+                    var facing = GetCurrentDirection();
+
+                    switch (facing)
+                    {
+                        case Direction.N:
+                            X += 1;
+                            break;
+                        case Direction.E:
+                            Y += 1;
+                            break;
+                        case Direction.S:
+                            X -= 1;
+                            break;
+                        case Direction.W:
+                            Y -= 1;
+                            break;
+                    }
                 }
 
-                if (command == 'B')
+                else if (command == 'B')
                 {
                     X -= 1;
                 }
 
-                if (command == 'L' || command == 'R')
+                else if (command == 'L' || command == 'R')
                 {
                     Turn(command);
                 }
--- tests/PlutoRover.UnitTests/PlutoRoverShould.cs
+++ tests/PlutoRover.UnitTests/PlutoRoverShould.cs
@@ -32,11 +32,11 @@ namespace PlutoRover.UnitTests
             rover.Orientation.Should().Be(0);
         }
 
-        [TestCase("L", -90)]
-        [TestCase("LL", -180)]
-        [TestCase("LLL", -270)]
-        [TestCase("LLLL", -360)]
-        [TestCase("LLLLL", -450)]
+        [TestCase("L", 270)]
+        [TestCase("LL", 180)]
+        [TestCase("LLL", 90)]
+        [TestCase("LLLL", 0)]
+        [TestCase("LLLLL", 270)]
         public void Turn_left(string command, int expectedOrientation)
         {
             var rover = new PlutoRover(0, 0, North);
@@ -51,9 +51,8 @@ namespace PlutoRover.UnitTests
         [TestCase("R", 90)]
         [TestCase("RR", 180)]
         [TestCase("RRR", 270)]
-        [TestCase("RRRR", 360)]
-        [TestCase("RRRRR", 450)]
-
+        [TestCase("RRRR", 0)]
+        [TestCase("RRRRR", 90)]
         public void Turn_right(string command, int expectedOrientation)
         {
             var rover = new PlutoRover(0, 0, North);
@@ -65,5 +64,20 @@ namespace PlutoRover.UnitTests
             rover.Orientation.Should().Be(expectedOrientation);
         }
 
+        [TestCase("FLFF", 1, -2, 270)]
+        [TestCase("FLFLFLFL", 0, 0, 0)]
+        [TestCase("RLRLRLRL", 0, 0, 0)]
+        [TestCase("FFBBFFLRLRLRFB", 2, 0, 0)]
+        [TestCase("FFLFFRFFLFF", 4, -4, 270)]
+        public void Complex_movement(string command, int expectedXCoordinate, int expectedYCoordinate, int expectedOrientation)
+        {
+            var rover = new PlutoRover(0, 0, North);
+
+            rover.Execute(command);
+
+            rover.X.Should().Be(expectedXCoordinate);
+            rover.Y.Should().Be(expectedYCoordinate);
+            rover.Orientation.Should().Be(expectedOrientation);
+        }
     }
 }
